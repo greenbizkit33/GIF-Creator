1012399796bb24c5746c3293daae8eb9920872b7
nathanhays33 <greenbizkit33@gmail.com>
2022-03-03 21:53:23 -0500

    WIP

--- app/src/main/java/com/nathanhaze/gifcreator/activity/SimpleSetupActivity.kt
+++ app/src/main/java/com/nathanhaze/gifcreator/activity/SimpleSetupActivity.kt
@@ -4,10 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.slider.RangeSlider
+import com.google.android.material.slider.Slider
 import com.nathanhaze.gifcreator.R
 import com.nathanhaze.gifcreator.manager.Utils
 import it.sephiroth.android.library.rangeseekbar.RangeSeekBar
 import kotlin.math.roundToInt
+import com.google.android.material.slider.LabelFormatter
+import java.lang.String
+
 
 class SimpleSetupActivity : AppCompatActivity() {
 
@@ -15,12 +20,15 @@ class SimpleSetupActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_simple_setup)
 
-        val frequencyRange = findViewById<RangeSeekBar>(R.id.frame_rate)
+        val frequencyRange = findViewById<Slider>(R.id.frame_rate)
 
+        frequencyRange.setLabelFormatter(LabelFormatter { value -> //It is just an example
+            (value/1000).toString()
+        })
         val createGifButton = findViewById<Button>(R.id.btn_create_gif)
 
         createGifButton.setOnClickListener {
-            Utils.frameFrequencyMilli = frequencyRange.keyProgressIncrement.roundToInt()
+            Utils.frameFrequencyMilli = frequencyRange.value.toInt()
             startActivity(Intent(this, GifCreatorActivity::class.java))
         }
 
